d8b44980f812286058f4579bfc8c3e1f6bc005fd
rkadiyali <37255909+rkadiyali@users.noreply.github.com>
2020-09-11 21:59:45 -0500

    Update CreateSystemUserComponent.java

--- CreateSystemUserComponent.java
+++ CreateSystemUserComponent.java
@@ -22,7 +22,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * @author Anirudh Sharma
+ * @author RK
  */
 @Component/*(immediate = true)*/
 public class CreateSystemUserComponent {
